2963a9194e27bfcd2010a63b9bc2fdf9171f081f
Nurbiike Sadykova <nurbiyke07@gmail.com>
2020-04-13 10:45:04 +0200

    created test files for login and register activities

---
+++ app/src/androidTest/java/com/tosp/sharingexpenses/RegisterActivityTest.java
@@ -0,0 +1,50 @@
+package com.tosp.sharingexpenses;
+
+import android.view.View;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.sharingexpenses.R;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
+public class RegisterActivityTest {
+
+    @Rule
+    public ActivityTestRule<RegisterActivity> RegisterActivityTestRule = new ActivityTestRule<RegisterActivity>(RegisterActivity.class);
+    private RegisterActivity mRegisterActivity = null;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mRegisterActivity = RegisterActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunch() {
+        View nameEditText = mRegisterActivity.findViewById(R.id.usernameEditText);
+        View emailEditText = mRegisterActivity.findViewById(R.id.emailEditText);
+        View passwordEditText = mRegisterActivity.findViewById(R.id.passwordEditText);
+        View pwConfirmEditText = mRegisterActivity.findViewById(R.id.confirmPWeditText);
+        View signUpBtn = mRegisterActivity.findViewById(R.id.signUpBtn);
+        View signInTextView = mRegisterActivity.findViewById(R.id.signInTextView);
+        assertNotNull(nameEditText);
+        assertNotNull(emailEditText);
+        assertNotNull(passwordEditText);
+        assertNotNull(pwConfirmEditText);
+        assertNotNull(signUpBtn);
+        assertNotNull(signInTextView);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
