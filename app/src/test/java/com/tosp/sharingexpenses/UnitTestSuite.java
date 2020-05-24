4984cd83a519cf81f55682d145a2190e5b03dd2d
Maksim Toropygin <toropyginmaxim@gmail.com>
2020-05-24 20:43:54 +0200

    Fixing bills testing

--- app/src/test/java/com/tosp/sharingexpenses/UnitTestSuite.java
+++
@@ -1,9 +0,0 @@
-package com.tosp.sharingexpenses;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-// Runs all unit tests.
-@RunWith(Suite.class)
-@Suite.SuiteClasses({PastBillListTest.class,
-        BillListTest.class})
-public class UnitTestSuite {}
